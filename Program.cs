7f005a594f8161301a992b62bba50c1aea7658e4
Ahmed yasir <ayasir@bwatech.sa>
2026-07-23 10:59:56 +0300

    feat: add balance endpoint

--- Program.cs
+++ Program.cs
@@ -5,8 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IBankService, BankService>();
 
 var app = builder.Build();
@@ -14,13 +16,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 app.MapControllers();
-
-
 
 app.Run();
 
